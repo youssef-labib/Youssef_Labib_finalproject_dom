5ff1183922dd387309ba524f0e3aceff31f6e6e3
youssef-labib <youssef.labib2023@gmail.com>
2025-05-28 16:07:50 +0100

    the imgs are not appearing without nodemodules

--- public/js/app.js
+++ public/js/app.js
@@ -203,16 +203,18 @@ for (let i = 0; i < specialsDishes.length; i++) {
 
 //~ Event Crousel
 
-let track = document.querySelector('.event-track')
+let track = document.querySelector(".event-track")
 let slides = Array.from(track.children)
-let indicators = document.querySelectorAll('.event-indicator')
+let indicators = document.querySelectorAll(".event-indicator")
+
 indicators.forEach(indicator => {
-    indicator.addEventListener('click', () => {
-    let index = parseInt(indicator.getAttribute('data-index'))
-    let slideWidth = slides[0].getBoundingClientRect().width
-      track.style.transform = `translateX(-${slideWidth * index}px)`
-        indicators.forEach(ind => ind.classList.remove('active'))
-        indicator.classList.add('active')
+    indicator.addEventListener("click", () => {
+        let index = parseInt(indicator.getAttribute("data-index"))
+        let slideWidth = slides[0].getBoundingClientRect().width
+
+        track.style.transform = `translateX(-${slideWidth * index}px)`
+        indicators.forEach(ind => ind.classList.remove("active"))
+        indicator.classList.add("active")
     });
 });
 
